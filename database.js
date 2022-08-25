f07349f85c4517597c14d276c192ff35d7b08b0d
spen-c-kimm <84422838+spen-c-kimm@users.noreply.github.com>
2022-08-25 11:18:00 -0500

    Updated host for database.js

--- database.js
+++ database.js
@@ -2,7 +2,7 @@ const mysql = require("mysql");
 require("dotenv").config();
 
 const connection = mysql.createConnection({
-    host: "localhost",
+    host: process.env.HOST,
     database: process.env.DATABASE,
     user: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD
