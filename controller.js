8c14f9e38a56b3ead0d46ab13e6f441b649122fe
spen-c-kimm <84422838+spen-c-kimm@users.noreply.github.com>
2022-08-25 15:32:49 -0500

    Created the initial signup function in the controller

--- controller.js
+++ controller.js
@@ -1,9 +1,16 @@
-const db = require("../database")
+const db = require("./database");
 
 const test = async function() {
     console.log("TEST")
 };
 
+const signup = async function(req, res) {
+    const user = await db.query("INSERT INTO users(fullName,userName,password) VALUES('Spencer Kimmell','skimmell','Password1!')")
+    console.log("user: ", user)
+    return res.send({ message: "Success!"});
+};
+
 module.exports = {
-    test
+    test,
+    signup
 }
