db41329e7763e068a218881758fcc68f70c84bb6
spen-c-kimm <84422838+spen-c-kimm@users.noreply.github.com>
2022-08-25 10:48:58 -0500

    Created env variables. Created database connection

---
+++ database.js
@@ -0,0 +1,19 @@
+const mysql = require("mysql");
+require("dotenv").config();
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    database: process.env.DATABASE,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD
+});
+
+connection.connect(function(error) {
+    if (error) {
+        throw error;
+    } else {
+        console.log("Database connected successfully!")
+    }
+});
+
+module.exports = connection;
