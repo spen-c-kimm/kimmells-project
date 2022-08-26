247b1ac4825738f9147539c9c4187a98240c3fd2
spen-c-kimm <84422838+spen-c-kimm@users.noreply.github.com>
2022-08-26 10:17:27 -0500

    Modified signup function

--- controller.js
+++ controller.js
@@ -1,13 +1,36 @@
-const db = require("./database");
+const mysqlQuery = require("./utilities").mysqlQuery;
 
-const test = async function() {
+const test = async function () {
     console.log("TEST")
 };
 
-const signup = async function(req, res) {
-    const user = await db.query("INSERT INTO users(fullName,userName,password) VALUES('Spencer Kimmell','skimmell','Password1!')")
-    console.log("user: ", user)
-    return res.send({ message: "Success!"});
+const signup = async function (req, res) {
+    try {
+        const params = req.body;
+
+        const existingUser = await mysqlQuery(`SELECT * FROM users WHERE userName = '${params.userName}'`);
+
+        if (existingUser?.length > 0) {
+            return res.send({
+                success: false,
+                message: "Username is already in use."
+            });
+        }
+
+        const newUser = await mysqlQuery(`INSERT INTO users(userName,fullName,password) VALUES('${params.userName}','${params.fullName}','${params.password}')`);
+
+        if (newUser) { return res.send({ success: true }) };
+
+        return res.send({
+            success: false,
+            message: "Could not create your profile. Please try again later."
+        });
+    } catch (error) {
+        return res.send({
+            success: false,
+            message: "There was an error while creating your profile. Please try again later."
+        });
+    }
 };
 
 module.exports = {
