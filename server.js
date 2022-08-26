44883d3f1db8102534a9363909ddafa714dd23b8
spen-c-kimm <84422838+spen-c-kimm@users.noreply.github.com>
2022-08-26 10:18:34 -0500

    Installed body-parser and added it to server.js

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const express = require("express");
 const path = require("path");
 const cors = require("cors");
+const bodyParser = require("body-parser");
 
 const router = require("./routes");
 
 const app = express();
+
+app.use(express.json());;
+// app.use(express.urlencoded({ extended: true, limit: "10kb" }));
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
 
 app.use(cors())
 
@@ -12,8 +22,8 @@ app.use(express.static(path.join(__dirname, "build")));
 
 app.use("/api/v1", router);
 
-app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "build", "index.html"))
-});
+// app.get("/", function(req, res) {
+//     res.sendFile(path.join(__dirname, "build", "index.html"))
+// });
 
 app.listen(3000);
