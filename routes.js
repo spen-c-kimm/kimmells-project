0977d2e3094af7f28267188940618c3e3f81c306
spen-c-kimm <84422838+spen-c-kimm@users.noreply.github.com>
2022-08-26 22:25:59 -0500

    Added login an validate session functions to the routes

--- routes.js
+++ routes.js
@@ -4,5 +4,7 @@ const router = express.Router();
 const controller = require("./controller");
 
 router.post("/signup", controller.signup);
+router.post("/login", controller.login);
+router.post("/validateSession", controller.validateSession);
 
 module.exports = router;
