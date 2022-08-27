9d090fd2efed0b3ada7127ad7f93c7f5827611ee
spen-c-kimm <84422838+spen-c-kimm@users.noreply.github.com>
2022-08-27 18:14:37 -0500

    Added create post and get feed methods to the routes

--- routes.js
+++ routes.js
@@ -6,5 +6,7 @@ const controller = require("./controller");
 router.post("/signup", controller.signup);
 router.post("/login", controller.login);
 router.post("/validateSession", controller.validateSession);
+router.post("/createPost", controller.createPost);
+router.post("/getFeed", controller.getFeed);
 
 module.exports = router;
