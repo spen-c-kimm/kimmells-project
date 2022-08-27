0f13fc13451a21de4127d83db3b727bf4558d119
spen-c-kimm <84422838+spen-c-kimm@users.noreply.github.com>
2022-08-27 18:15:01 -0500

    Updated utllities.js

--- utilities.js
+++ utilities.js
@@ -4,6 +4,7 @@ async function mysqlQuery(sql) {
     return new Promise((resolve, reject) => {
         try {
             db.query(sql, function (err, result, fields) {
+                if (err) resolve(null);
                 return resolve(result);
             });
         } catch(error) {
