d72cd907d4fc3f4a08d448810f6f8e42a4057024
spen-c-kimm <84422838+spen-c-kimm@users.noreply.github.com>
2022-08-27 18:15:42 -0500

    Created getFeed, getUserPosts, andcreatePost methods

--- controller.js
+++ controller.js
@@ -17,19 +17,18 @@ const signup = async function (req, res) {
 
         const encryptedPassword = await bcrypt.hash(params.password, 12);
 
-        const newUser = await mysqlQuery(`INSERT INTO users(userName,fullName,password) VALUES('${params.userName}','${params.fullName}','${encryptedPassword}')`);
+        await mysqlQuery(`INSERT INTO users(userName,fullName,password) VALUES('${params.userName}','${params.fullName}','${encryptedPassword}')`);
+        const newUser = (await mysqlQuery(`SELECT * FROM users WHERE ID = LAST_INSERT_ID()`))[0];
 
         if (newUser) {
-            const token = jwt.sign({
-                data: 'foobar'
-            }, 'secret', { expiresIn: '1h' });
-            return res.send({ success: true, token });
+            return res.send({ success: true });
+        } else {
+            return res.send({
+                success: false,
+                message: "Could not create your profile. Please try again later."
+            });
         };
 
-        return res.send({
-            success: false,
-            message: "Could not create your profile. Please try again later."
-        });
     } catch (error) {
         return res.send({
             success: false,
@@ -42,7 +41,7 @@ const login = async function (req, res) {
     try {
         const params = req.body;
 
-        const user = await mysqlQuery(`SELECT * FROM users WHERE userName = '${params.userName}'`);
+        const user = (await mysqlQuery(`SELECT * FROM users WHERE userName = '${params.userName}'`))[0];
 
         if (!user) {
             return res.send({ success: false, message: "Username not registered." });
@@ -55,7 +54,7 @@ const login = async function (req, res) {
 
         if (isAuthenticated) {
             const token = jwt.sign({
-                data: 'foobar'
+                user
             }, 'secret', { expiresIn: '1h' });
             return res.send({ success: true, token });
         }
@@ -86,7 +85,89 @@ const validateSession = async function (req, res) {
     } catch (error) {
         return res.send({
             success: false,
-            message: "There was an error while logging into your profile. Please try again later."
+            message: "There was an error while validating your session. Please try again later."
+        });
+    }
+};
+
+const getFeed = async function (req, res) {
+    try {
+        const posts = await mysqlQuery(`
+        SELECT P.text, P.dateCreated, U.fullName, U.userName, U.ID AS userID
+        FROM posts AS P
+        JOIN users AS U On U.ID = P.userID
+        WHERE P.repliedToID = 0
+        ORDER BY P.dateCreated DESC
+        `);
+
+        if (posts) {
+            return res.send({ success: true, posts });
+        } else {
+            return res.send({ success: false, message: "No posts found." });
+        }
+    } catch (error) {
+        return res.send({
+            success: false,
+            message: "There was an error while loading your feed. Please try again later."
+        });
+    }
+};
+
+const getUserPosts = async function (req, res) {
+    try {
+        const params = req.body;
+        const userID = params?.userID;
+
+        const posts = await mysqlQuery(`
+        SELECT P.text, P.dateCreated, U.fullName, U.userName, P.userId,
+        P2.text AS repliedToText, U2.fullName AS repliedToFullName, U2.userName AS repliedToUserName,
+        U2.ID AS repliedToUserID
+        FROM posts AS P
+        JOIN users AS U ON U.ID = P.userID
+        LEFT JOIN posts AS P2 ON P2.ID = P.repliedToID
+        LEFT JOIN users AS U2 ON U2.ID = P2.userID
+        WHERE P.userID = ${userID} 
+        ORDER BY P.dateCreated DESC
+        `);
+
+        if (posts) {
+            return res.send({ success: true, posts });
+        } else {
+            return res.send({ success: false, message: "No posts found." });
+        }
+    } catch (error) {
+        return res.send({
+            success: false,
+            message: "There was an error while loading your feed. Please try again later."
+        });
+    }
+};
+
+const createPost = async function (req, res) {
+    try {
+        const params = req.body;
+        const token = params.token;
+
+        const decoded = jwt.verify(token, 'secret');
+        const user = decoded?.user;
+
+        if (!user) {
+            return res.send({ success: false, message: "Could not create the post. Please try again later." });
+        }
+
+        await mysqlQuery(`INSERT INTO posts(userID,text,repliedToID) VALUES('${user.ID}','${params.text}','${params.repliedToID}')`);
+        const newPost = (await mysqlQuery(`SELECT * FROM posts WHERE ID = LAST_INSERT_ID()`))[0];
+
+        if (newPost) {
+            return res.send({ success: true, newPost });
+        } else {
+            return res.send({ success: false, message: "Could not create the post. Please try again later." });
+        }
+
+    } catch (error) {
+        return res.send({
+            success: false,
+            message: "There was an error while creating the post. Please try again later."
         });
     }
 };
@@ -94,5 +175,8 @@ const validateSession = async function (req, res) {
 module.exports = {
     signup,
     login,
-    validateSession
+    validateSession,
+    getFeed,
+    getUserPosts,
+    createPost
 }
