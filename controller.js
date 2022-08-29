5d5154ae83cf1560004b0dbec3ce84369f00ccf8
spen-c-kimm <84422838+spen-c-kimm@users.noreply.github.com>
2022-08-28 19:33:28 -0500

    Created likePost action. Updated query in the getFeed action

--- controller.js
+++ controller.js
@@ -92,10 +92,16 @@ const validateSession = async function (req, res) {
 
 const getFeed = async function (req, res) {
     try {
+        const params = req.body;
+        const userID = params?.userID;
+
         const posts = await mysqlQuery(`
-        SELECT P.text, P.dateCreated, U.fullName, U.userName, P.userID
+        SELECT P.text, P.dateCreated, U.fullName, 
+        U.userName, P.userID, P.ID AS postID,
+        CASE WHEN L.deleted = 0 THEN 1 ELSE 0 END AS liked 
         FROM posts AS P
         JOIN users AS U On U.ID = P.userID
+        LEFT JOIN likes AS L ON L.userID = ${userID} AND L.postID = P.ID
         WHERE P.repliedToID = 0
         ORDER BY P.dateCreated DESC
         `);
@@ -123,13 +129,15 @@ const getUserPosts = async function (req, res) {
         const user = (await mysqlQuery(`SELECT * FROM users WHERE ID =${userID}`))[0];
 
         const posts = await mysqlQuery(`
-        SELECT P.text, P.dateCreated, U.fullName, U.userName, P.userId,
-        P2.text AS repliedToText, U2.fullName AS repliedToFullName, U2.userName AS repliedToUserName,
-        U2.ID AS repliedToUserID
+        SELECT P.text, P.dateCreated, U.fullName, U.userName, P.userId, P.ID AS postID,
+        P2.text AS repliedToText, U2.fullName AS repliedToFullName, 
+        U2.userName AS repliedToUserName, P2.ID AS repliedToPostID,
+        U2.ID AS repliedToUserID, CASE WHEN L.deleted = 0 THEN 1 ELSE 0 END AS liked 
         FROM posts AS P
         JOIN users AS U ON U.ID = P.userID
         LEFT JOIN posts AS P2 ON P2.ID = P.repliedToID
         LEFT JOIN users AS U2 ON U2.ID = P2.userID
+        LEFT JOIN likes AS L ON L.userID = ${userID} AND L.postID = P.ID
         WHERE P.userID = ${userID} 
         ORDER BY P.dateCreated DESC
         `);
@@ -176,11 +184,33 @@ const createPost = async function (req, res) {
     }
 };
 
+const likePost = async function (req, res) {
+    try {
+        const params = req.body;
+
+        const like = (await mysqlQuery(`SELECT * FROM likes WHERE userID = ${params.userID} AND postID = ${params.postID}`))[0];
+
+        if (like) {
+            const deleted = like.deleted;
+            await mysqlQuery(`Update likes SET deleted = ${!deleted} WHERE userID = ${params.userID} AND postID = ${params.postID}`);
+        } else {
+            await mysqlQuery(`INSERT INTO likes(userID,postID) VALUES('${params.userID}','${params.postID}')`);
+        }
+
+    } catch (error) {
+        return res.send({
+            success: false,
+            message: "There was an error while creating the post. Please try again later."
+        });
+    }
+};
+
 module.exports = {
     signup,
     login,
     validateSession,
     getFeed,
     getUserPosts,
-    createPost
+    createPost,
+    likePost
 }
