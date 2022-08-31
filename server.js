53e8234eb7b0f743d9054c9c921fc8da498f4850
spen-c-kimm <84422838+spen-c-kimm@users.noreply.github.com>
2022-08-30 22:59:29 -0500

    Manually added routes so that it would work correctly with the static files

--- server.js
+++ server.js
@@ -22,8 +22,25 @@ app.use(express.static(path.join(__dirname, "build")));
 
 app.use("/api/v1", router);
 
-// app.get("/", function(req, res) {
-//     res.sendFile(path.join(__dirname, "build", "index.html"))
-// });
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "build", "index.html"))
+});
+
+app.get("/signup", function(req, res) {
+    res.sendFile(path.join(__dirname, "build", "index.html"))
+});
+
+app.get("/feed", function(req, res) {
+    res.sendFile(path.join(__dirname, "build", "index.html"))
+});
+
+app.get("/profile/:userID", function(req, res) {
+    res.sendFile(path.join(__dirname, "build", "index.html"))
+});
+
+app.get("/replies/:postID", function(req, res) {
+    res.sendFile(path.join(__dirname, "build", "index.html"))
+});
+
 
 app.listen(3000);
